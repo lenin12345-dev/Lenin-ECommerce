52c4ed4440ff03719cc8969dec4555f8a4f7ae15
leninm <leninm@lecturenotes.in>
2023-07-15 01:57:21 +0530

    hero sectionfix

--- src/app/component/herosection.js
+++ src/app/component/herosection.js
@@ -56,14 +56,12 @@ export default function HeroSection() {
         </Grid>
         <Grid item xs={4}sm={12} md={12} lg={7} xl={7}>
           <Box className = {classes.heroImage} display='flex' justifyContent='flex-end'>
-            <React.Suspense>
 
         <img
                 src="images/hero.jpg"
                 alt="hero-section-photo"
                 style={{ width:'80%'}}
               />
-            </React.Suspense>
 
               <Box sx={{    position: 'absolute',
       top: -70,
